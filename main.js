9f0ca77b64f1590c7e996194376fb1ce6ec3c1f6
swish8596 <165816997+swish8596@users.noreply.github.com>
2025-01-12 20:45:25 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var login = prompt('Введите свое имя')
+console.log(login);
+
+var age = prompt('Введите свой возраст')
+console.log(age + ' лет');
+
+var primer1 = prompt(' решите пример 120 + 45 ')
+console.log('Пример 1) 120 + 45 = ' + (120 + 45) + ' Ваш ответ ' + primer1 )
+
+var primer2 = prompt(' решите пример 120 - 45 ')
+console.log('Пример 2) 120 - 45 = ' + (120 - 45) + ' Ваш ответ ' + primer2 )
+
+var primer3 = prompt(' решите пример 120 * 45 ')
+console.log('Пример 3) 120 * 45 = ' + 120 * 45 + ' Ваш ответ ' + primer3 )
+
+var primer4 = prompt(' решите пример 120 / 45 ')
+console.log('Пример 4) 120 / 45 = ' + 120 / 45 + ' Ваш ответ ' + primer4 )
+
+var primer5 = prompt(' решите пример 120 % 45 ')
+console.log('Пример 5) 120 % 45 = ' + 120 % 45 + ' Ваш ответ ' + primer5 )
+
+alert('Зайдите в консоль для просмотра результатов')
